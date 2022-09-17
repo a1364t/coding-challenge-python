e97bee29a94ff27d7da5017fd54f243401e696ad
Alireza <ali1985reza@gmail.com>
2022-09-17 18:40:26 +0430

    m

--- myApp/views.py
+++ myApp/views.py
@@ -4,3 +4,10 @@ from rest_framework.response import Response
 @api_view()
 def hello_world(request):
     return Response({"message": "hello_world"})
+
+@api_view(['GET','POST'])
+def hello(request):
+    if request.method == "GET":
+        return Response({"message": "hello_world"})
+    elif request.method == "POST":
+        return Response({"message": "Hello, {}".format(request.data["name"])})
