d72a542a18ec39243e309b780b86546a1c94fa42
Alireza <ali1985reza@gmail.com>
2022-09-17 19:46:22 +0430

    url

--- myApp/urls.py
+++ myApp/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from myApp import views
 
 urlpatterns = [
-    path('hello-world', views.hello_world),
-    path('hello', views.hello)
+    path('', views.app)
 ]
--- myApp/views.py
+++ myApp/views.py
@@ -1,13 +1,22 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 
-@api_view()
-def hello_world(request):
-    return Response({"message": "hello_world"})
 
-@api_view(['GET','POST'])
-def hello(request):
-    if request.method == "GET":
-        return Response({"message": "hello_world"})
-    elif request.method == "POST":
-        return Response({"message": "Hello, {}".format(request.data["name"])})
+
+@api_view(['POST'])
+def app(request):
+    try:
+        image = request.data["image"]
+        slug = request.data["slug"]
+        title = request.data["title"]
+    except:
+        return Response({"error": "Could not decode request: JSON parsing failed"}, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        return Response({"response": 
+            [
+            {"image", image, 
+            "slug", slug, 
+            "title", title}
+            ]
+        }, status=status.HTTP_200_OK)
